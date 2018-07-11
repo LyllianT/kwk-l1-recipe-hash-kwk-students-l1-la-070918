fbe259c8b1ffbd00adc4d868aa920345bb3e3bb1
LyllianT <lilyterrific2008@gmail.com>
2018-07-11 20:46:47 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -23,7 +23,7 @@ perfect_10_recipe = [
   :slivered_almonds_lightly_toasted => "1/4 cup",
   :mini_dark_chocolate_chips => "3/4 cup",
   :olive_oil => "1/2 cup",
-  :agave - "1/4 cup",
+  :agave => "1/4 cup"
   ]
   
 
