7a3a973255dd19e43f9f2e8551fc0acd10e3eaed
LyllianT <lilyterrific2008@gmail.com>
2018-07-11 20:45:26 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -13,8 +13,9 @@
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
-perfect_10_recipe = {
+perfect_10_recipe = [
   :almond_flour => "2 cup" 
+  :gluten_freewhole_oats => "1 cup"
   :kosher_salt => "1/2 tea spoon"
   :baking_powder => "1/2 tea spoon"
   :baking_soda => "1/4 tea spoon"
@@ -23,7 +24,7 @@ perfect_10_recipe = {
   :mini_dark_chocolate_chips => "3/4 cup"
   :olive_oil => "1/2 cup"
   :agave - "1/4 cup"
-  }
+  ]
   
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
