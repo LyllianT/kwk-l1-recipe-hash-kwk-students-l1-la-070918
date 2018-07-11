8dc4b1da6bf6da33798a93aa472158a5218dd6c1
LyllianT <lilyterrific2008@gmail.com>
2018-07-11 21:00:39 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -29,17 +29,7 @@ perfect_10_recipe = [
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
-   perfect_10_recipe = [
-  :almond_flour => "2 cup",
-  :gluten_freewhole_oats => "1 cup",
-  :kosher_salt => "1/2 tea spoon",
-  :baking_powder => "1/2 tea spoon",
-  :baking_soda => "1/4 tea spoon",
-  :Xanthan_Gum => "1/2 tea spoon",
-  :slivered_almonds_lightly_toasted => "1/4 cup",
-  :mini_dark_chocolate_chips => "3/4 cup",
-  :olive_oil => "1/2 cup",
-  :agave => "1/4 cup"
+   
   ]
 end
 
@@ -61,4 +51,5 @@ end
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
 def number_ingredients(perfect_10_recipe)
+  
 end
